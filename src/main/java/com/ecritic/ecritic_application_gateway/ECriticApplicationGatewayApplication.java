bc4973206d041600d8accea897c45cc2f9a69da3
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-05-25 16:39:01 -0300

    Adding project files

---
+++ src/main/java/com/ecritic/ecritic_application_gateway/ECriticApplicationGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ecritic.ecritic_application_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECriticApplicationGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECriticApplicationGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecritic/ecritic_application_gateway/ECriticApplicationGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecritic.ecritic_application_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECriticApplicationGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
