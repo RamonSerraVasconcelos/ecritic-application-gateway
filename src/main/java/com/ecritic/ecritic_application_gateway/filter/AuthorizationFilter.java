1b7262034a1b803bb7a37dc3a205bd4496192054
Ramon <rserravasconcelos@gmail.com>
2025-04-26 18:04:41 -0300

    Returning correct error when final route is unavailable

--- src/main/java/com/ecritic/ecritic_application_gateway/filter/AuthorizationFilter.java
+++ src/main/java/com/ecritic/ecritic_application_gateway/filter/AuthorizationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import com.ecritic.ecritic_application_gateway.properties.ServiceProperties;
@@ -40,24 +41,25 @@ public class AuthorizationFilter implements GatewayFilter {
                 .baseUrl(servicesProperties.getEcriticAuthenticationServiceAddress())
                 .build();
 
+        return introspectToken(webClient, accessToken)
+                .flatMap(authorizationDataResponse -> {
+                    exchange.getRequest().mutate().headers(headers -> headers.set(HttpHeaders.AUTHORIZATION, authorizationDataResponse.getAccessToken()));
+
+                    log.info("Retrieved authorization data from ecritic-authentication-service: [{}]", authorizationDataResponse);
+
+                    return chain.filter(exchange);
+                });
+    }
+
+    private Mono<AuthorizationData> introspectToken(WebClient webClient, String accessToken) {
         return webClient.post()
                 .uri("/auth/token/introspect")
                 .header(HttpHeaders.AUTHORIZATION, accessToken)
                 .retrieve()
                 .bodyToMono(AuthorizationData.class)
-                .flatMap(authorizationDataResponse -> {
-                    exchange.getRequest().mutate()
-                            .headers(headers -> headers.set(HttpHeaders.AUTHORIZATION, authorizationDataResponse.getAccessToken()));
-
-                    log.info("Retrieved authorization data from ecritic-authentication-service: [{}]", authorizationDataResponse);
-
-                    return chain.filter(exchange);
-                })
                 .onErrorResume(throwable -> {
                     log.error("Failed to retrieve authorization data from ecritic-authentication-service", throwable);
-
-                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                    return exchange.getResponse().setComplete();
+                    return Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, throwable.getMessage()));
                 });
     }
 
